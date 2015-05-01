029ca2e31b90d027656d6592e448318e54c9c0a8
Bhargav Panth <bhargavrpanth@gmail.com>
2015-05-01 14:52:02 +0530

    created thankYou.php

---
+++ thankYou.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>OnRusticRoutes</title>
+</head>
+<body>
+<?php require 'partials/navbar.php' ?>
+
+<br>
+<br>
+<br>
+<br>
+<div class="sucess">
+	<h2>Awesome, we're good to go !</h2>
+	<h4>Thank you for your registrations. We can't wait to see you on board</h4>
+</div>
+<div class="fail">
+	<h2>Oops, there seems to be some issue with the registration</h2>
+	<h4>We're sorry, there seems to be some error. Please try again in a few minutes</h4>
+</div>
+<?php require 'partials/footer.php' ?>	
+</body>
+</html>
