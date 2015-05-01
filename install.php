a088b35812cb0cd1de9de4461d6010bca73b4523
Nishchal Gautam <gautam.nishchal@gmail.com>
2015-05-01 12:29:04 +0530

    preparation of index file

--- install.php
+++ install.php
@@ -1,4 +1,19 @@
 <?php
-	$sql="CREATE TABLE tracks();";
-
+	$test=<<<HEAD
+	CREATE TABLE PlanYourWeekEnd(
+		id INT PRIMARY KEY AUTO_INCREMENT,
+		fname VARCHAR(100),
+		lname VARCHAR(100),
+		email VARCHAR(255),
+		dob DATE,
+		sex VARCHAR(10),
+		address TEXT,
+		city VARCHAR(255),
+		state VARCHAR(100),
+		country VARCHAR(100),
+		mobile VARCHAR(15),
+		referer VARCHAR(200)
+		);
+HEAD;
+require_once("config.php");
 ?>
