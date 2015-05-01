9d120e2e4ed90b662bca9263b273d74a1754e7b4
Bhargav Panth <bhargavrpanth@gmail.com>
2015-05-01 12:35:05 +0530

    Changed images and product_index

--- partials/image_grid.php
+++ partials/image_grid.php
@@ -3,13 +3,12 @@
 	<link rel="stylesheet" type="text/css" href="../css/grid_component.css" />
 	<ul class="grid">
 		 <!-- <li><a href="#"><img src="../img/thumbs/manali.png" style='width:100%;' border="0" alt="cover" /><div><h3>Hampi</h3></div></a></li> -->
-		<li><a href="#"><img src="../img/thumbs/7.png" style='width:100%' border="0" alt=""><div><h3>Goa</h3></div></a></li>
-		<li><a href="#"><img src="../img/thumbs/7.png" style='width:100%' border="0" alt=""><div><h3>Goa</h3></div></a></li>
-		<li><a href="#"><img src="../img/thumbs/7.png" style='width:100%' border="0" alt=""><div><h3>Goa</h3></div></a></li>
-		<li><a href="#"><img src="../img/thumbs/7.png" style='width:100%' border="0" alt=""><div><h3>Goa</h3></div></a></li>
-		<li><a href="#"><img src="../img/thumbs/7.png" style='width:100%' border="0" alt=""><div><h3>Goa</h3></div></a></li>
-		<li><a href="#"><img src="../img/thumbs/7.png" style='width:100%' border="0" alt=""><div><h3>Goa</h3></div></a></li>
-		<li><a href="#"><img src="../img/thumbs/manali.png" style='width:100%;' border="0" alt="cover" /><div><h3>Manali</h3></div></a></li>
+		<li><a href="#"><img src="../img/thumbs/7.jpg" style='width:100%' border="0" alt=""><div><h3>Goa</h3></div></a></li>
+		<li><a href="#"><img src="../img/thumbs/7.jpg" style='width:100%' border="0" alt=""><div><h3>Goa</h3></div></a></li>
+		<li><a href="#"><img src="../img/thumbs/7.jpg" style='width:100%' border="0" alt=""><div><h3>Goa</h3></div></a></li>
+		<li><a href="#"><img src="../img/thumbs/7.jpg" style='width:100%' border="0" alt=""><div><h3>Goa</h3></div></a></li>
+		<li><a href="#"><img src="../img/thumbs/7.jpg" style='width:100%' border="0" alt=""><div><h3>Goa</h3></div></a></li>
+		<li><a href="#"><img src="../img/thumbs/7.jpg" style='width:100%' border="0" alt=""><div><h3>Goa</h3></div></a></li>
 		
 <!-- 		<li><a href="#"><img src="../img/thumbs/ker.png" style='width:100%' border="0" alt=""><div><h3>Goa</h3></div></a></li>
 		<li><a href="#"><img src="../img/thumbs/ker.png" style='width:100%' border="0" alt=""><div><h3>Goa</h3></div></a></li> -->			
--- product_index.php
+++ product_index.php
@@ -8,11 +8,14 @@
 	<?php require 'partials/navbar.php' ?> <br><br><br><br><br>
 	<img src="img/cover.jpg" style="width:100%" alt="cover">
 	<hr>
-	<!-- Images Grid -->
+	
 		<center>
 	<h2>What we offer</h2>
 		</center>
 	
+	<!-- Images Grid -->
+	<?php require 'partials/image_grid.php' ?>
+
 
 </body>
 </html>
