5a94ffaadfed75e0d7120b153d0dc6182a0347c2
Bhargav Panth <bhargavrpanth@gmail.com>
2015-05-08 19:52:44 +0530

    Some more changes that i don't remember

---
+++ planYourTrip.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<link rel="shortcut icon" href="img/small_logo.png" />
+	<title>onrustiroutes.com</title>
+</head>
+<body>
+	<?php require 'partials/navbar.php' ?>
+		
+		<h3 >PLAN YOUR WEEKEND</h3>
+		<div>
+			<form action="" method="POST">
+				
+			</form>
+		</div>
+	<?php require 'partials/foter.php' ?>
+</body>
+</html>
